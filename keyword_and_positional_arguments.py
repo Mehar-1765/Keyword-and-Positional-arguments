c671655041971e1762eae351f4e928f80bf9d8bb
Mehar-1765 <147295909+Mehar-1765@users.noreply.github.com>
2026-06-22 09:51:56 +0530

    Add files via upload

---
+++ keyword_and_positional_arguments.py
@@ -0,0 +1,19 @@
+'''def details(id,name,mail):
+    id=10
+    name='mehar'
+    mail='m@gmail.com'
+    print(id,name,mail)
+details(id='id',name='name',mail='mail')'''
+
+'''def details(id,name,mail):
+    print(id,name,mail)
+details(id='id',name='name',mail='mail')
+details(id=20,name='mehar',mail='m@gmail.com')
+details(id=30,name='begum',mail='b@gmail.com')
+details(40,'anas','a@gmail.com')
+details('erishad','e@gmail.com',70)
+details(mail='ak@gmail.com',id=80,name='akhil')'''
+
+
+
+
